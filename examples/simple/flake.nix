8de9b2090a8fc7ad0905040cffdd87df732b1507
nandi <nandithebull@outlook.com>
2026-04-04 03:38:46 -0700

    Revert simple example to standard flake format (no flake-parts)

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -1,48 +1,39 @@
 {
   description = "Simple NCP Example Project";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    ncp.url = "github:codegod100/ncp";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = inputs@{ self, nixpkgs, flake-parts, ncp }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ncp.flakeModule ];
-      
-      systems = [ "x86_64-linux" ];
-      
-      ncp.containers = {
-        backend = {
-          port = 9001;
-          containerPort = 80;
-          config = { config, pkgs, ... }: {
-            services.nginx = {
-              enable = true;
-              virtualHosts.default = {
-                default = true;
-                locations."/".return = builtins.readFile ./backend-response.txt;
-              };
+  outputs = { self, nixpkgs }: {
+    ncp.containers = {
+      backend = {
+        port = 9001;
+        containerPort = 80;
+        config = { config, pkgs, ... }: {
+          services.nginx = {
+            enable = true;
+            virtualHosts.default = {
+              default = true;
+              locations."/".return = builtins.readFile ./backend-response.txt;
             };
-            networking.firewall.allowedTCPPorts = [ 80 ];
           };
+          networking.firewall.allowedTCPPorts = [ 80 ];
         };
-        
-        frontend = {
-          port = 9002;
-          containerPort = 80;
-          config = { config, pkgs, ... }: {
-            services.nginx = {
-              enable = true;
-              virtualHosts.default = {
-                default = true;
-                locations."/".return = builtins.readFile ./frontend.html;
-              };
+      };
+      
+      frontend = {
+        port = 9002;
+        containerPort = 80;
+        config = { config, pkgs, ... }: {
+          services.nginx = {
+            enable = true;
+            virtualHosts.default = {
+              default = true;
+              locations."/".return = builtins.readFile ./frontend.html;
             };
-            networking.firewall.allowedTCPPorts = [ 80 ];
           };
+          networking.firewall.allowedTCPPorts = [ 80 ];
         };
       };
     };
+  };
 }
