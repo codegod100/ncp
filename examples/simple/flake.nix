10fea2ff085d358e356fb984612cd11509f6ff1f
nandi <nandithebull@outlook.com>
2026-04-04 02:49:44 -0700

    Fix frontend escaping with simpler config

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -23,12 +23,14 @@
       frontend = {
         port = 9002;
         containerPort = 80;
-        config = { config, pkgs, ... }: {
+        config = { config, pkgs, lib, ... }: {
           services.nginx = {
             enable = true;
             virtualHosts.default = {
               default = true;
-              locations."/".return = ''200 "<h1>Frontend</h1><p>This is the frontend container</p><p><a href=\"http://204.168.220.202:9001/\">Go to Backend</a></p>"';
+              locations."/".extraConfig = ''
+                return 200 "Frontend - Hello!";
+              '';
             };
           };
           networking.firewall.allowedTCPPorts = [ 80 ];
