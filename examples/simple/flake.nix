8c26b3fd0f7fbab7ef3d6a2429f94d2169645655
nandi <nandithebull@outlook.com>
2026-04-04 02:49:31 -0700

    Fix frontend config - simplify with embedded HTML in nginx return

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -24,15 +24,11 @@
         port = 9002;
         containerPort = 80;
         config = { config, pkgs, ... }: {
-          system.activationScripts.createHtml = ''
-            mkdir -p /var/www
-            echo '<h1>Frontend</h1><button onclick="fetch(\'http://204.168.220.202:9001/\').then(r=>r.json()).then(d=>alert(JSON.stringify(d)))">Test</button>' > /var/www/index.html
-          '';
           services.nginx = {
             enable = true;
             virtualHosts.default = {
               default = true;
-              root = "/var/www";
+              locations."/".return = ''200 "<h1>Frontend</h1><p>This is the frontend container</p><p><a href=\"http://204.168.220.202:9001/\">Go to Backend</a></p>"';
             };
           };
           networking.firewall.allowedTCPPorts = [ 80 ];
