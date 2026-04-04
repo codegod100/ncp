4a5e7fb30e265adeb1618bdcb7d40630250a63aa
nandi <nandithebull@outlook.com>
2026-04-04 03:54:54 -0700

    Fix simple example: add firewall.allowedTCPPorts for port 80

--- examples/simple/flake.nix
+++ examples/simple/flake.nix
@@ -3,40 +3,52 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   
   outputs = { self, nixpkgs }: {
-    # Export containers as nixosConfigurations for nixos-container --flake
     nixosConfigurations.backend = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [{
+      modules = [({ pkgs, ... }: {
         boot.isContainer = true;
         networking.useDHCP = false;
-        networking.firewall.enable = true;
+        networking.firewall = {
+          enable = true;
+          allowedTCPPorts = [ 80 ];
+        };
         services.nginx = {
           enable = true;
           virtualHosts.default = {
             default = true;
-            locations."/".return = builtins.readFile ./backend-response.txt;
+            locations."/" = {
+              extraConfig = ''
+                return 200 '{"message":"Hello from backend"}';
+              '';
+            };
           };
         };
-      }];
+      })];
     };
     
     nixosConfigurations.frontend = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [{
+      modules = [({ pkgs, ... }: {
         boot.isContainer = true;
         networking.useDHCP = false;
-        networking.firewall.enable = true;
+        networking.firewall = {
+          enable = true;
+          allowedTCPPorts = [ 80 ];
+        };
         services.nginx = {
           enable = true;
           virtualHosts.default = {
             default = true;
-            locations."/".return = builtins.readFile ./frontend.html;
+            locations."/" = {
+              extraConfig = ''
+                return 200 '<h1>Hello from frontend</h1>';
+              '';
+            };
           };
         };
-      }];
+      })];
     };
     
-    # Also export ncp.containers for the API to read ports
     ncp.containers = {
       backend = { port = 9001; containerPort = 80; };
       frontend = { port = 9002; containerPort = 80; };
